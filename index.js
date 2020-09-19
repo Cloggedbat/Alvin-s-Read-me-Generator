fe4d33b9a5f849501da0b70c430564542c8e669c
cleminator <alvinclemens@gmail.com>
2020-09-18 22:06:56 -0600

    RIP RBG

--- index.js
+++ index.js
@@ -1,34 +1,57 @@
 const fs = require("fs");
 const gm = require("./utils/generateMarkdown")
-
 // array of questions for user
-const questions = [
-{
-    type:"input",
-    message:"what is your name?",
-    title:"name"
+questions
+.prompt ([
+    {
+        type: "input",
+        message: "what is your name?",
+        title: "name"
         
-      },
-
-];
+    },
+    {
+        type: "input",
+        message: "what is your name?",
+        title: "name"
+        
+    },
+    {
+        type: "input",
+        message: "what is your name?",
+        title: "name"
+        
+    },
+    {
+        type: "input",
+        message: "what is your name?",
+        title: "name"
+        
+    },
+    
+    
+])
+const questions = require("questions")
+.then(function(responce){
+    console.log(responce)
+})
 
 // function to write README file
 function writeToFile(fileName, data) {
-console.log(fileName, data)
-        fs.writeFile(fileName,(data), function(err){
-            if (err) {
+    console.log(fileName, data)
+    fs.writeFile(fileName, (data), function (err) {
+        if (err) {
             throw err;
         };
-     
+        
     })
 }
 
 // function to initialize program
 function init() {
-const title = gm ({
-    title: "what up"
-})
-writeToFile ("README.md",(title))    
+    const title = gm({
+        title: "what up"
+    })
+    writeToFile("README.md", (title))
 
 }
 
@@ -37,4 +60,3 @@ init();
 
 
 
-  
